8eb0aa42ea43d638897cf7c9047a8e77b6cf7840
Andrei Avram <avramandrei@ymail.com>
2021-10-23 18:21:58 +0300

    All levels are maps.
    Allows recursion.

--- include/msd/poly_map.hpp
+++ include/msd/poly_map.hpp
@@ -12,140 +12,6 @@
 namespace msd {
 
 /**
- * Polymorphic map
- *
- * @tparam Key First type of key.
- * @tparam Keys Other types of keys.
- */
-template <typename Key, typename... Keys>
-class poly_map {
-    struct poly_map_element;
-
-   public:
-    /**
-     * Checked access by key.
-     *
-     * @tparam T Type of key.
-     *
-     * @param key Key to index map by.
-     *
-     * @return Map at given key.
-     *
-     * @throws std::out_of_range if key not found.
-     */
-    template <typename T>
-    [[nodiscard]] poly_map_element& at(const T& key)
-    {
-        return elements_.at(key);
-    }
-
-    /**
-     * Checked access by list of keys.
-     *
-     * @tparam T Type of first key.
-     * @tparam Ts Types of other keys.
-     *
-     * @param key First key to index map by.
-     * @param keys Other keys.
-     *
-     * @return Map at given key.
-     *
-     * @throws std::out_of_range if key not found.
-     */
-    template <typename T, typename... Ts>
-    [[nodiscard]] auto& at(const T& key, const Ts&... keys)
-    {
-        return elements_.at(key).at(keys...);
-    }
-
-    /**
-     * Checked access by key. Const overload.
-     *
-     * @tparam T Type of key.
-     *
-     * @param key Key to index map by.
-     *
-     * @return Map at given key.
-     *
-     * @throws std::out_of_range if key not found.
-     */
-    template <typename T, typename... Ts>
-    [[nodiscard]] auto& at(const T& key, const Ts&... keys) const
-    {
-        return const_cast<poly_map*>(this)->at(key, keys...);
-    }
-
-    /**
-     * Unchecked access by key.
-     *
-     * @tparam T Type of key.
-     *
-     * @param key Key to index map by.
-     *
-     * @return Map at given key.
-     *
-     * @throws std::out_of_range if key not found.
-     */
-    template <typename T>
-    auto& operator[](const T& key)
-    {
-        return elements_[key];
-    }
-
-    /**
-     * Iterate over map element by given visitor.
-     *
-     * @param visitor Visitor with overloads for keys types.
-     */
-    template <typename V>
-    void for_each(V&& visitor)
-    {
-        elements_.for_each(std::forward<V>(visitor));
-    }
-
-    /**
-     * Iterate over map element by given visitor. Const overload.
-     *
-     * @param visitor Visitor with overloads for keys types.
-     */
-    template <typename V>
-    void for_each(V&& visitor) const
-    {
-        const_cast<poly_map*>(this)->for_each(std::forward<V>(visitor));
-    }
-
-    /**
-     * Tests if map has no elements.
-     */
-    [[nodiscard]] bool empty() const noexcept { return elements_.empty(); }
-
-    /**
-     * Returns number of elements.
-     */
-    [[nodiscard]] std::size_t size() const noexcept { return elements_.size(); }
-
-    /**
-     * Removes elements from map.
-     */
-    void clear() noexcept { elements_.elements_.clear(); }
-
-    /**
-     * Tests if given path of keys is in map.
-     *
-     * @param key Key to search for.
-     * @param keys List of keys to search for.
-     */
-    template <typename T, typename... Ts>
-    [[nodiscard]] bool contains(const T& key, const Ts&... keys) const
-    {
-        return elements_.contains(key, keys...);
-    }
-
-   private:
-    poly_map_element elements_;
-};
-
-/**
  * Map value
  */
 struct poly_map_value {
@@ -192,15 +58,14 @@ struct poly_map_value {
 };
 
 /**
- * Child map
+ * Polymorphic map
  *
  * @tparam Key First type of key.
- * @tparam Keys Other types of keys.
+ * @tparam Keys Other optional types of keys.
  */
 template <typename Key, typename... Keys>
-struct poly_map<Key, Keys...>::poly_map_element {
-    std::map<std::variant<Key, Keys...>, poly_map_element> elements_;
-
+class poly_map {
+   public:
     /**
      * Assign value to map.
      *
@@ -219,8 +84,6 @@ struct poly_map<Key, Keys...>::poly_map_element {
     /**
      * Checked access by key.
      *
-     * @tparam T Type of key.
-     *
      * @param key Key to index map by.
      *
      * @return Map at given key.
@@ -236,9 +99,6 @@ struct poly_map<Key, Keys...>::poly_map_element {
     /**
      * Checked access by list of keys.
      *
-     * @tparam T Type of first key.
-     * @tparam Ts Types of other keys.
-     *
      * @param key First key to index map by.
      * @param keys Other keys.
      *
@@ -247,15 +107,28 @@ struct poly_map<Key, Keys...>::poly_map_element {
      * @throws std::out_of_range if key not found.
      */
     template <typename T, typename... Ts>
-    [[nodiscard]] poly_map_element& at(const T& key, const Ts&... keys)
+    [[nodiscard]] poly_map& at(const T& key, const Ts&... keys)
     {
         return elements_.at(key).at(keys...);
     }
 
     /**
-     * Unchecked access by key.
+     * Checked access by key. Const overload.
      *
-     * @tparam T Type of key.
+     * @param key Key to index map by.
+     *
+     * @return Map at given key.
+     *
+     * @throws std::out_of_range if key not found.
+     */
+    template <typename T, typename... Ts>
+    [[nodiscard]] auto& at(const T& key, const Ts&... keys) const
+    {
+        return const_cast<poly_map*>(this)->at(key, keys...);
+    }
+
+    /**
+     * Unchecked access by key.
      *
      * @param key Key to index map by.
      *
@@ -313,7 +186,7 @@ struct poly_map<Key, Keys...>::poly_map_element {
     template <typename V>
     void for_each(V&& visitor) const
     {
-        const_cast<poly_map_element*>(this)->for_each(std::forward<V>(visitor));
+        const_cast<poly_map*>(this)->for_each(std::forward<V>(visitor));
     }
 
     /**
@@ -367,7 +240,24 @@ struct poly_map<Key, Keys...>::poly_map_element {
         return elements_.at(key).contains(keys...);
     }
 
+    /**
+     * Access map element at next level
+     *
+     * @pre Map must not be empty: !empty().
+     * @return Reference to map.
+     */
+    poly_map& map() { return elements_.begin()->second; }
+
+    /**
+     * Access map element at next level.  Const overload.
+     *
+     * @pre Map must not be empty: !empty().
+     * @return Const reference to map.
+     */
+    const poly_map& map() const { return elements_.cbegin()->second; }
+
    private:
+    std::map<std::variant<Key, Keys...>, poly_map> elements_;
     poly_map_value value_;
 };
 
